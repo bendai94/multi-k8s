5fde86f15e30f91c651e42bffe2bc1f659b57ca0
bendai94 <bendai94@gmail.com>
2023-02-24 16:58:22 -0500

    updated deploy.sh for latest images

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,13 @@
-docker build -t bendai94/multi-client-10-14 -f ./client/Dockerfile ./client
-docker build -t bendai94/multi-server-10-14 -f ./server/Dockerfile ./server
-docker build -t bendai94/multi-worker-10-14 -f ./worker/Dockerfile ./worker
-docker push bendai94/multi-client-10-14
-docker push bendai94/multi-server-10-14
-docker push bendai94/multi-worker-10-14
+docker build -t bendai94/multi-client-10-14:latest -t bendai94/multi-client-10-14:$SHA -f ./client/Dockerfile ./client
+docker build -t bendai94/multi-server-10-14:latest -t bendai94/multi-server-10-14:$SHA -f ./server/Dockerfile ./server
+docker build -t bendai94/multi-worker-10-14:latest -t bendai94/multi-worker-10-14:$SHA -f ./worker/Dockerfile ./worker
+docker push bendai94/multi-client-10-14:latest
+docker push bendai94/multi-server-10-14:latest
+docker push bendai94/multi-worker-10-14:latest
+docker push bendai94/multi-client-10-14:$SHA
+docker push bendai94/multi-server-10-14:$SHA
+docker push bendai94/multi-worker-10-14:$SHA
 
-kubectl set image deployment/multi-client-deployment client=bendai94/multi-client:latest
+kubectl set image deployment/multi-client-deployment client=bendai94/multi-client:$SHA
+kubectl set image deployment/multi-server-deployment server=bendai94/multi-server:$SHA
+kubectl set image deployment/multi-worker-deployment worker=bendai94/multi-worker:$SHA
